b7ec58348672c76152924f46ef54f7c32ba82c20
Matteo Manchi <matteo.manchi@gmail.com>
2017-10-29 22:55:01 +0100

    Login text

--- src/screen/Login.js
+++ src/screen/Login.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Expo from 'expo';
 import {View, Text, Image, TouchableOpacity} from 'react-native';
-import {Platform, StyleSheet} from 'react-native';
+import {Platform, StyleSheet, Dimensions} from 'react-native';
 import * as Constants from '../common/constants';
 
 export default class Login extends Component {
@@ -13,8 +13,42 @@ export default class Login extends Component {
                 </View>
                 <Image
                     source={require('../res/login/bg-cinema.png')}
-                    style={styles.contentContainer}
-                />
+                    style={styles.contentContainer}>
+                    <View style={styles.textContainer}>
+                        <Text style={[styles.textCommon, styles.text1]}>
+                            Hai 18 anni?
+                        </Text>
+                        <Text style={[styles.textCommon]}>Per te</Text>
+                        <Text style={[styles.textCommon, styles.text3]}>
+                            500€
+                        </Text>
+                        <Text style={[styles.textCommon]}>da spendere in</Text>
+                        <Text style={[styles.textCommon, styles.text4]}>
+                            cinema
+                        </Text>
+                    </View>
+
+                    <View style={styles.bottomButtons}>
+                        <TouchableOpacity>
+                            <Text
+                                style={[
+                                    styles.textCommon,
+                                    styles.bottomButton,
+                                ]}>
+                                INFORMAZIONI
+                            </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Text
+                                style={[
+                                    styles.textCommon,
+                                    styles.bottomButton,
+                                ]}>
+                                NON HAI SPID?
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </Image>
                 <TouchableOpacity onPress={this.props.onLogin}>
                     <View style={styles.login}>
                         <Text style={styles.loginText}>ENTRA CON SPID</Text>
@@ -47,7 +81,51 @@ const styles = StyleSheet.create({
     contentContainer: {
         flex: 1,
         resizeMode: 'cover',
+        alignItems: 'center',
     },
+    textContainer: {
+        flex: 1,
+        paddingTop: 45,
+    },
+
+    textCommon: {
+        fontSize: 16,
+        lineHeight: 18,
+        textAlign: 'center',
+        backgroundColor: 'transparent',
+        color: Constants.WHITE,
+        fontFamily: Constants.TITILLIUM_REGULAR,
+    },
+
+    text1: {
+        fontSize: 32,
+        fontFamily: Constants.TITILLIUM_BOLD,
+        lineHeight: 38,
+    },
+    text3: {
+        fontSize: 32,
+        fontFamily: Constants.TITILLIUM_BOLD,
+        lineHeight: 38,
+    },
+    text4: {
+        fontSize: 40,
+        color: Constants.WHITE,
+        fontStyle: 'italic',
+        lineHeight: 45,
+        marginTop: 40,
+    },
+
+    bottomButtons: {
+        width: Dimensions.get('screen').width,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginBottom: 25,
+    },
+
+    bottomButton: {
+        fontFamily: Constants.TITILLIUM_BOLD,
+    },
+
     login: {
         height: 64,
         backgroundColor: Constants.PRIMARY_BLUE,
